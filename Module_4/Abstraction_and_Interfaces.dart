f53d1b8122cb34cb6c58b9ee159780cd79b6c4fa
Abdul_Kaiyum <kaiyum3744@gmail.com>
2026-09-17 21:49:16 +0600

    Abstrac_class

--- Module_4/Abstraction_and_Interfaces.dart
+++ Module_4/Abstraction_and_Interfaces.dart
@@ -1,3 +1,46 @@
-void main() {
-  
+abstract class Bank{
+    String name ="BANGLADESH BANK";
+    void deposit(double amount);
+    void withdrow(double amount);
+    void chakblance();
+
+}
+
+class IBBL implements Bank{
+    @override
+    String name="islamic bank bangladesh lmet";
+
+    double _blance =1;
+    @override
+    void deposit(double amount){
+        _blance +=amount;
+    print("IBBL: $amount depodit");
+    }
+
+    @override
+    void withdrow(double amount){
+        if(amount<= _blance)
+        {
+            _blance -=amount;
+            print('IBBL: $amount WITHDROW');
+        }
+        else{
+            print("IBBL: INSUFFICIENT BLANCE");
+        }
+    }
+    @override
+    void chakblance() {
+    // TODO: implement chakblance
+    print("IBBL COUNT BLANCE :$_blance TK");
+  }
+
+}
+void main(){
+    Bank obj = IBBL();
+    print(obj.name);
+    obj.deposit(55);
+    obj.withdrow(33);
+    obj.chakblance();
+    
+
 }
